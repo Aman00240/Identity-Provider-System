2195e1e4dcb689f0c9d645f915dfecb2c517ddd5
Aman Saini <amankf200@gmail.com>
2026-09-24 23:33:07 +0530

    feat: Added jwt token creation

--- app/config.py
+++ app/config.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    ALGORITHM: str = "RS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    PRIVATE_KEY: str = (BASE_DIR / "certs" / "private.pem").read_text()
+    PUBLIC_KEY: str = (BASE_DIR / "certs" / "public.pem").read_text()
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,9 @@
+from datetime import datetime, timedelta, timezone
+
 import bcrypt
+import jwt
+
+from app.config import settings
 
 
 def get_password_hash(password: str) -> str:
@@ -14,3 +19,22 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     hashed_password_bytes = hashed_password.encode("utf-8")
 
     return bcrypt.checkpw(plain_password_bytes, hashed_password_bytes)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode.update({"exp": expire, "iat": datetime.now(timezone.utc)})
+
+    encoded_jwt = jwt.encode(
+        to_encode, settings.PRIVATE_KEY, algorithm=settings.ALGORITHM
+    )
+
+    return encoded_jwt
